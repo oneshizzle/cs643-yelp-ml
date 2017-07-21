79a3817d9239c24d8d0c3efb2a47b37fb9cc6b05
Tri Phan <tp231t@att.com>
2017-07-20 20:21:23 -0700

    [Tri Phan] I add 3 more classes: Checkin, Tip and User.

---
+++ src/main/java/edu/njit/cs643/group7/model/Tip.java
@@ -0,0 +1,49 @@
+package edu.njit.cs643.group7.model;
+
+public class Tip {
+
+	private String text;
+	private String date;
+	private int likes;
+	private String business_id;
+	private String user_id;
+	private String type;
+	
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public int getLikes() {
+		return likes;
+	}
+	public void setLikes(int likes) {
+		this.likes = likes;
+	}
+	public String getBusiness_id() {
+		return business_id;
+	}
+	public void setBusiness_id(String business_id) {
+		this.business_id = business_id;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+}
---
+++ src/main/java/edu/njit/cs643/group7/model/User.java
@@ -0,0 +1,170 @@
+package edu.njit.cs643.group7.model;
+
+import java.util.List;
+
+public class User {
+	
+	private String user_id;
+	private String name;
+	private int review_count;
+	private String yelping_since;
+	private List<String> friends;
+	private int useful;
+	private int funny;
+	private int cool;
+	private int fans;
+	private List<Integer> elite;
+	private Double average_stars;
+	private int compliment_hot;
+	private int compliment_more;
+	private int compliment_profile;
+	private int compliment_cute;
+	private int compliment_list;
+	private int compliment_note;
+	private int compliment_plain;
+	private int compliment_cool;
+	private int compliment_funny;
+	private int compliment_writer;
+	private int compliment_photos;
+	private String type;
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getReview_count() {
+		return review_count;
+	}
+	public void setReview_count(int review_count) {
+		this.review_count = review_count;
+	}
+	public String getYelping_since() {
+		return yelping_since;
+	}
+	public void setYelping_since(String yelping_since) {
+		this.yelping_since = yelping_since;
+	}
+	public List<String> getFriends() {
+		return friends;
+	}
+	public void setFriends(List<String> friends) {
+		this.friends = friends;
+	}
+	public int getUseful() {
+		return useful;
+	}
+	public void setUseful(int useful) {
+		this.useful = useful;
+	}
+	public int getFunny() {
+		return funny;
+	}
+	public void setFunny(int funny) {
+		this.funny = funny;
+	}
+	public int getCool() {
+		return cool;
+	}
+	public void setCool(int cool) {
+		this.cool = cool;
+	}
+	public int getFans() {
+		return fans;
+	}
+	public void setFans(int fans) {
+		this.fans = fans;
+	}
+	public List<Integer> getElite() {
+		return elite;
+	}
+	public void setElite(List<Integer> elite) {
+		this.elite = elite;
+	}
+	public Double getAverage_stars() {
+		return average_stars;
+	}
+	public void setAverage_stars(Double average_stars) {
+		this.average_stars = average_stars;
+	}
+	public int getCompliment_hot() {
+		return compliment_hot;
+	}
+	public void setCompliment_hot(int compliment_hot) {
+		this.compliment_hot = compliment_hot;
+	}
+	public int getCompliment_more() {
+		return compliment_more;
+	}
+	public void setCompliment_more(int compliment_more) {
+		this.compliment_more = compliment_more;
+	}
+	public int getCompliment_profile() {
+		return compliment_profile;
+	}
+	public void setCompliment_profile(int compliment_profile) {
+		this.compliment_profile = compliment_profile;
+	}
+	public int getCompliment_cute() {
+		return compliment_cute;
+	}
+	public void setCompliment_cute(int compliment_cute) {
+		this.compliment_cute = compliment_cute;
+	}
+	public int getCompliment_list() {
+		return compliment_list;
+	}
+	public void setCompliment_list(int compliment_list) {
+		this.compliment_list = compliment_list;
+	}
+	public int getCompliment_note() {
+		return compliment_note;
+	}
+	public void setCompliment_note(int compliment_note) {
+		this.compliment_note = compliment_note;
+	}
+	public int getCompliment_plain() {
+		return compliment_plain;
+	}
+	public void setCompliment_plain(int compliment_plain) {
+		this.compliment_plain = compliment_plain;
+	}
+	public int getCompliment_cool() {
+		return compliment_cool;
+	}
+	public void setCompliment_cool(int compliment_cool) {
+		this.compliment_cool = compliment_cool;
+	}
+	public int getCompliment_funny() {
+		return compliment_funny;
+	}
+	public void setCompliment_funny(int compliment_funny) {
+		this.compliment_funny = compliment_funny;
+	}
+	public int getCompliment_writer() {
+		return compliment_writer;
+	}
+	public void setCompliment_writer(int compliment_writer) {
+		this.compliment_writer = compliment_writer;
+	}
+	public int getCompliment_photos() {
+		return compliment_photos;
+	}
+	public void setCompliment_photos(int compliment_photos) {
+		this.compliment_photos = compliment_photos;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	
+}
