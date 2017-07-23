09ba70537439d09ab162587ab2c905d3727e6d9a
USER <USER@Marsha.home>
2017-07-23 19:18:25 -0400

    adding test class to test reading of json file into objects

---
+++ src/main/java/edu/njit/cs643/group7/util/BusinessUtils.java
@@ -0,0 +1,18 @@
+package edu.njit.cs643.group7.util;
+
+import java.util.Arrays;
+
+import edu.njit.cs643.group7.model.Business;
+
+public class BusinessUtils {
+
+	public static Boolean isRestaurant(Business aBusiness) {
+		Boolean isRestaurant = false;
+		for (String category : aBusiness.getCategories()) {
+			if (Arrays.asList("food", "restaurants").contains(category.toLowerCase()))
+				isRestaurant = true;
+		}
+		return isRestaurant;
+	}
+
+}
---
+++ src/test/java/edu/njit/cs643/group7/model/ValidateModel.java
@@ -0,0 +1,42 @@
+package edu.njit.cs643.group7.model;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+
+import edu.njit.cs643.group7.util.BusinessUtils;
+
+public class ValidateModel {
+
+	@Test
+	public void testBusinessModel() {
+		Gson gson = new Gson();
+		List<Business> businesses = new ArrayList<Business>();
+
+		try (Reader reader = new InputStreamReader(
+				ClassLoader.getSystemResourceAsStream("data/sample_business_json"))) {
+			JsonReader jsonReader = new JsonReader(reader);
+			jsonReader.setLenient(true);
+			// Convert JSON to Java Object
+			while (jsonReader.hasNext() && jsonReader.peek() != JsonToken.END_DOCUMENT) {
+				Business aBusiness = gson.fromJson(jsonReader, Business.class);
+				System.out.println(aBusiness.getName() + " " + BusinessUtils.isRestaurant(aBusiness));
+				businesses.add(aBusiness);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		Assert.assertTrue(businesses.size() == 11);
+	}
+
+}
