db99c2ff57d0a6a2734f023f04674d5fa53d62e4
USER <USER@Marsha.home>
2017-07-24 14:10:41 -0400

    fixing business model

--- src/main/java/edu/njit/cs643/group7/YelpRR.java
+++ src/main/java/edu/njit/cs643/group7/YelpRR.java
@@ -3,13 +3,14 @@
  */
 package edu.njit.cs643.group7;
 
-import org.apache.avro.ipc.specific.Person;
+import org.apache.spark.api.java.function.FilterFunction;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoder;
 import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.SparkSession;
 
 import edu.njit.cs643.group7.model.Business;
+import edu.njit.cs643.group7.util.BusinessUtils;
 
 /**
  * Yelp Restaurant Recommender App
@@ -28,10 +29,14 @@ public class YelpRR {
 		spark = SparkSession.builder().appName(APP_NAME).master("local[2]").config("spark.executor.memory", "1g")
 				.config("spark.some.config.option", "some-value").getOrCreate();
 		Encoder<Business> businessEncoder = Encoders.bean(Business.class);
-		
+
 		Dataset<Business> businessDS = spark.read()
 				.json(ClassLoader.getSystemResource("data/sample_business_json").getPath()).as(businessEncoder);
+		Dataset<Business> restaurantDS = businessDS
+				.filter((FilterFunction<Business>) aBusiness -> BusinessUtils.isRestaurant(aBusiness));
 		businessDS.show();
+
+		restaurantDS.show();
 	}
 
 }
--- src/main/java/edu/njit/cs643/group7/model/Business.java
+++ src/main/java/edu/njit/cs643/group7/model/Business.java
@@ -1,8 +1,9 @@
 package edu.njit.cs643.group7.model;
 
+import java.io.Serializable;
 import java.util.List;
 
-public class Business {
+public class Business implements Serializable {
 
 	private String business_id;
 	private String name;
@@ -11,15 +12,18 @@ public class Business {
 	private String city;
 	private String state;
 	private String postal_code;
-	private String latitude;
-	private String longitude;
-	private String stars;
-	private String review_count;
-	private String is_open;
+	private Double latitude;
+	private Double longitude;
+	private Double stars;
+	private Long review_count;
+	private Long is_open;
 	private List<String> attributes;
 	private List<String> categories;
 	private List<String> hours;
 	private String type;
+
+	public Business() {
+	}
 
 	public String getBusiness_id() {
 		return business_id;
@@ -77,43 +81,43 @@ public class Business {
 		this.postal_code = postalCode;
 	}
 
-	public String getLatitude() {
+	public Double getLatitude() {
 		return latitude;
 	}
 
-	public void setLatitude(String latitude) {
+	public void setLatitude(Double latitude) {
 		this.latitude = latitude;
 	}
 
-	public String getLongitude() {
+	public Double getLongitude() {
 		return longitude;
 	}
 
-	public void setLongitude(String longitude) {
+	public void setLongitude(Double longitude) {
 		this.longitude = longitude;
 	}
 
-	public String getStars() {
+	public Double getStars() {
 		return stars;
 	}
 
-	public void setStars(String stars) {
+	public void setStars(Double stars) {
 		this.stars = stars;
 	}
 
-	public String getReview_count() {
+	public Long getReview_count() {
 		return review_count;
 	}
 
-	public void setReview_count(String review_count) {
+	public void setReview_count(Long review_count) {
 		this.review_count = review_count;
 	}
 
-	public String getIs_open() {
+	public Long getIs_open() {
 		return is_open;
 	}
 
-	public void setIs_open(String isOpen) {
+	public void setIs_open(Long isOpen) {
 		this.is_open = isOpen;
 	}
 
@@ -147,6 +151,11 @@ public class Business {
 
 	public void setType(String type) {
 		this.type = type;
+	}
+
+	@Override
+	public String toString() {
+		return "Business {" + "name='" + name + '\'' + ", stars='" + stars + '\'' + '}';
 	}
 
 }
